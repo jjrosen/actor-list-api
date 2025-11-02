aff8ff8fa54e46269bf599f5da4df7191e4b4c8a
jjrosen <joshrosen281@gmail.com>
2025-11-02 15:45:21 -0600

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "faker"
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do
+  Actor.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    known_for: Faker::Movie.title
+  )
+end
