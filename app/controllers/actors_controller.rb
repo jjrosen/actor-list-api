d2ef0e31647db05aa0e96694ba07d39a21fcd803
jjrosen <joshrosen281@gmail.com>
2025-11-02 16:04:19 -0600

    create show method

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,11 @@
 class ActorsController < ApplicationController
   def index
-    actors = Actor.all
-    render formulate: actors
+    @actors = Actor.all
+    render template: "actors/index"
+  end
+
+  def show
+    @actor = Actor.find(params[:id])
+    render template: "actors/show"
   end
 end
