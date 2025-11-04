a7b901e91c3463cf3a058146fcd074cff6156ee5
jjrosen <joshrosen281@gmail.com>
2025-11-03 21:10:31 -0600

    test for index

--- test/controllers/actors_controller_test.rb
+++ test/controllers/actors_controller_test.rb
@@ -4,4 +4,20 @@ class ActorsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index" do 
+    get "/actors.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Actor.count, data.count
+  end
+
+
+
+
+
+
+
+
 end
